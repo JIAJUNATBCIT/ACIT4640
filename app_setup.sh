47255af0899206e0d63db1960d33bca5a3a57b10
JIAJUNATBCIT <49431626+JIAJUNATBCIT@users.noreply.github.com>
2020-09-24 17:53:47 -0700

    update URLs

--- app_setup.sh
+++ app_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 # download script from the cloud and create script file
-ssh todoapp sudo curl https://raw.githubusercontent.com/JIAJUNATBCIT/ACIT4640/master/install.sh -o /home/admin/install.sh
+ssh todoapp sudo curl https://raw.githubusercontent.com/JIAJUNATBCIT/ACIT4640/module02/setup/install.sh -o /home/admin/install.sh
 # Run locally
 ssh todoapp sudo chmod 777 /home/admin/install.sh
 ssh todoapp sudo chown admin:admin /home/admin/install.sh
