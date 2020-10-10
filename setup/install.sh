d54dbdf6a5962df3d3dcd6186b149e8a4a817463
JIAJUNATBCIT <49431626+JIAJUNATBCIT@users.noreply.github.com>
2020-10-09 22:04:06 -0700

    Update install.sh

--- setup/install.sh
+++ setup/install.sh
@@ -7,7 +7,7 @@ sudo sh -c 'echo P@ssw0rd | passwd todoapp --stdin'
 #Add todoapp user to sudoers group
 sudo usermod -aG wheel todoapp
 # install Mongodb
-sudo curl https://raw.githubusercontent.com/JIAJUNATBCIT/ACIT4640/master/mongodb-org-4.4.repo -o /etc/yum.repos.d/mongodb-org-4.4.repo
+sudo curl https://raw.githubusercontent.com/JIAJUNATBCIT/ACIT4640/module02/setup/mongodb-org-4.4.repo -o /etc/yum.repos.d/mongodb-org-4.4.repo
 #sudo dnf search mongodb
 sudo dnf install -y -b mongodb-org
 # start mongodb
@@ -33,7 +33,7 @@ sudo npm install
 # install nginx
 sudo dnf install -y nginx
 # import nginx conf from git
-sudo curl https://raw.githubusercontent.com/JIAJUNATBCIT/ACIT4640/master/nginx.conf -o /etc/nginx/nginx.conf
+sudo curl https://raw.githubusercontent.com/JIAJUNATBCIT/ACIT4640/module02/setup/nginx.conf -o /etc/nginx/nginx.conf
 # start nginx
 sudo systemctl enable nginx
 sudo systemctl start nginx
@@ -49,7 +49,7 @@ cd ~
 sudo chmod a+rx /home/todoapp/
 sudo chown todoapp:todoapp /home/todoapp/ACIT4640-todo-app/
 # Import Deamon conf from Github to target machine [ROOT]
-sudo curl https://raw.githubusercontent.com/JIAJUNATBCIT/ACIT4640/master/todoapp.service -o /etc/systemd/system/todoapp.service
+sudo curl https://raw.githubusercontent.com/JIAJUNATBCIT/ACIT4640/module02/setup/todoapp.service -o /etc/systemd/system/todoapp.service
 # Reload and start todoapp Deamon
 sudo systemctl daemon-reload
 sudo systemctl enable todoapp
