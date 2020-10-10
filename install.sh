bd3f09a43030942bf1e29ade9557606b7b92a57a
JIAJUNATBCIT <49431626+JIAJUNATBCIT@users.noreply.github.com>
2020-10-10 14:21:18 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -52,8 +52,20 @@ sudo dnf install -y -b nodejs
 sudo npm install
 # install nginx
 sudo dnf install -y nginx
-# import nginx conf from git
-sudo curl https://raw.githubusercontent.com/JIAJUNATBCIT/ACIT4640/master/nginx.conf -o /etc/nginx/nginx.conf
+# config nginx
+sudo sed -i 's:/usr/share/nginx/html;:/home/todoapp/ACIT4640-todo-app/public;:' $NGINX_CONF
+if grep -qF "location /api/todos" $NGINX_CONF; then
+	echo "Nginx file already configured!"
+else
+	sudo sed -i '49 i \ \ \ \ \ \ \ \ location /api/todos{proxy_pass http://localhost:8080;}' $NGINX_CONF
+fi
+# start nginx
+sudo systemctl enable nginx
+sudo systemctl start nginx
+echo "nginx installed and started"
+# disable SE Linux
+sudo setenforce 0
+sudo sed -r -i 's/SELINUX=(enforcing|disabled)/SELINUX=permissive/' /etc/selinux/config
 # start nginx
 sudo systemctl enable nginx
 sudo systemctl start nginx
@@ -68,8 +80,23 @@ sudo firewall-cmd --runtime-to-permanent
 cd ~
 sudo chmod a+rx /home/todoapp/
 sudo chown todoapp:todoapp /home/todoapp/ACIT4640-todo-app/
-# Config todoapp as a daemon
-sudo curl https://raw.githubusercontent.com/JIAJUNATBCIT/ACIT4640/master/todoapp.service -o /etc/systemd/system/todoapp.service
+cat <<EOF > todoapp.service
+[Unit]
+Description=Todo app, ACIT4640
+After=network.target
+Requires=mongod.service
+[Service]
+Environment=NODE_PORT=8080
+WorkingDirectory=/home/todoapp/ACIT4640-todo-app
+Type=simple
+User=$USER
+ExecStartPre=/bin/sleep 5
+ExecStart=node /home/todoapp/ACIT4640-todo-app/server.js
+Restart=always
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo mv todoapp.service /etc/systemd/system/
 # Reload and start todoapp Deamon
 sudo systemctl daemon-reload
 sudo systemctl enable todoapp
