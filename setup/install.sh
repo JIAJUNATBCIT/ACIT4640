ebc482e5839c6fc60116c2679021cbee1fb9811e
JIAJUNATBCIT <49431626+JIAJUNATBCIT@users.noreply.github.com>
2020-10-10 01:38:00 -0700

    Update install.sh

--- setup/install.sh
+++ setup/install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -x
+USER="todoapp"
+DIR="/home/todoapp/app"
+NGINX_CONF="/etc/nginx/nginx.conf"
 #add todoapp user
 sudo useradd todoapp
 #set password to todoapp user
@@ -6,7 +9,15 @@ sudo sh -c 'echo P@ssw0rd | passwd todoapp --stdin'
 #Add todoapp user to sudoers group
 sudo usermod -aG wheel todoapp
 # install Mongodb
-sudo curl https://raw.githubusercontent.com/JIAJUNATBCIT/ACIT4640/module02/setup/mongodb-org-4.4.repo -o /etc/yum.repos.d/mongodb-org-4.4.repo
+cat <<EOF > mongodb-org-4.4.repo
+[mongodb-org-4.4]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/\$releasever/mongodb-org/4.4/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.4.asc
+EOF
+sudo mv mongodb-org-4.4.repo /etc/yum.repos.d/mongodb-org-4.4.repo
 #sudo dnf search mongodb
 sudo dnf install -y -b mongodb-org
 # start mongodb
@@ -25,14 +36,25 @@ sudo git clone https://github.com/timoguic/ACIT4640-todo-app.git
 # navigate to the project folder
 cd ./ACIT4640-todo-app
 # Reconfig MongoDB path
-sudo sh -c 'echo "module.exports = {localUrl: \"mongodb://localhost/acit4640\"};" > ./config/database.js'
+sudo su - $USER -c "rm -rf $DIR/config/database.js"
+sudo su - $USER -c "cat <<EOF > database.js 
+module.exports = {
+    localUrl: 'mongodb://localhost/ACIT4640'
+}; 
+EOF"
+sudo su - $USER -c "mv database.js $DIR/config/database.js"
 # install project packages
 sudo dnf install -y -b nodejs
 sudo npm install
 # install nginx
 sudo dnf install -y nginx
 # import nginx conf from git
-sudo curl https://raw.githubusercontent.com/JIAJUNATBCIT/ACIT4640/module02/setup/nginx.conf -o /etc/nginx/nginx.conf
+sudo sed -i 's:/usr/share/nginx/html;:/home/todoapp/app/public;:' $NGINX_CONF
+if grep -qF "location /api/todos" $NGINX_CONF; then
+	echo "Nginx file already configured!"
+else
+	sudo sed -i '49 i \ \ \ \ \ \ \ \ location /api/todos{\n \ \ \ \ \ \ \ \ \ \ \ \ proxy_pass http://localhost:8080;\n \ \ \ \ \ \ \ \}' $NGINX_CONF
+fi
 # start nginx
 sudo systemctl enable nginx
 sudo systemctl start nginx
@@ -48,7 +70,23 @@ cd ~
 sudo chmod a+rx /home/todoapp/
 sudo chown todoapp:todoapp /home/todoapp/ACIT4640-todo-app/
 # Import Deamon conf from Github to target machine [ROOT]
-sudo curl https://raw.githubusercontent.com/JIAJUNATBCIT/ACIT4640/module02/setup/todoapp.service -o /etc/systemd/system/todoapp.service
+cat <<EOF > todoapp.service
+[Unit]
+Description=Todo app, ACIT4640
+After=network.target
+Requires=mongod.service
+[Service]
+Environment=NODE_PORT=8080
+WorkingDirectory=$DIR
+Type=simple
+User=$USER
+ExecStartPre=/bin/sleep 5
+ExecStart=/usr/bin/node $DIR/server.js
+Restart=always
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo mv todoapp.service /etc/systemd/system/todoapp.service
 # Reload and start todoapp Deamon
 sudo systemctl daemon-reload
 sudo systemctl enable todoapp
